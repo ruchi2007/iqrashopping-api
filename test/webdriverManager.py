16ef1b39d2dd0e43031c5076b84e176a3df23ca7
ruchi2007 <ruchi2007usa@gmail.com>
2023-01-25 22:50:08 -0500

    add file

--- test/webdriverManager.py
+++ test/webdriverManager.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-driver= webdriver.Chrome(ChromeDriverManager().install())
-#url='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-#url.driver = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',webdriver.Chrome(ChromeDriverManager().install()
-username,password="admin","admi123"
-#print(username)
-assert username=="admin"
-assert username !="admin"
-#driver.get(url)
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+url='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+#url.driver = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',webdriver.Chrome(ChromeDriverManager().install())
+username, password = 'Admin', 'admin123'
+print(username)
+
+assert username == 'Admin'
+assert username != 'Admin'
+driver.get(url)
--- webdriverManager.py
+++ webdriverManager.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 driver= webdriver.Chrome(ChromeDriverManager().install())
-#url='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-#url.driver = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',webdriver.Chrome(ChromeDriverManager().install()
-username,password="admin","admi123"
-#print(username)
-assert username=="admin"
-assert username !="admin"
-#driver.get(url)
+url='https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+#url.driver = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',webdriver.Chrome(ChromeDriverManager().install())
+username,password="Admin","admin123"
+print(username)
+driver.get(url)
+assert username == "Admin"
+assert username !="Admin"
